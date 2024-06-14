6452aa28030ddd1d2a833359fc2180fd3e2d8e44
Clara Arguello <clara.arguello@gmail.com>
2024-06-14 19:48:06 -0300

    changes

--- 02-Condicionales/index.js
+++ 02-Condicionales/index.js
@@ -1,69 +1,69 @@
-// let edad = 18
-// let altura = 200
-// let color = 'celeste'
+let edad = 18
+let altura = 200
+let color = 'celeste'
 
-// if(altura < 150){
-//     console.log('Sos petiso')
-// }else if(altura >150){
-//     console.log('Sos normal')
-// }else if(altura >160){
-//     console.log('Sos alto')
-// } else{
-//     console.log('Sos altisimo!')
-// }
-
-
-// if(altura > 160){
-//     console.log('sos alto')
-// }
-// if(edad >=18){
-//     console.log('sos mayor de edad')
-// }
+if(altura < 150){
+    console.log('Sos petiso')
+}else if(altura >150){
+    console.log('Sos normal')
+}else if(altura >160){
+    console.log('Sos alto')
+} else{
+    console.log('Sos altisimo!')
+}
 
 
-
-
-// if(edad>=18){
-//     console.log('sos mayor de edad')
-// }else{
-//     console.log('no tenes la edad minima requerida')
-// }
+if(altura > 160){
+    console.log('sos alto')
+}
+if(edad >=18){
+    console.log('sos mayor de edad')
+}
 
 
 
-// let hola = document.querySelector('div')
-// console.log("🚀 ~ div:", hola)
 
-// hola.innerText('HOLA CHICOS DEL GRUPO 2')
-
-
-// let temp = prompt('Ingrese la temperatura en grqados celcius')
-// if(temp>30){
-//     console.log('Hace mucho calor')
-// }else if(temp>21){
-//     console.log('Hace calor')
-// }else if(temp>11){
-//     console.log('Esta agradable')
-// }else if(temp>0){
-//     console.log('HAce frio')
-// }else {
-//     console.log('Hace mucho frio')
-// }
+if(edad>=18){
+    console.log('sos mayor de edad')
+}else{
+    console.log('no tenes la edad minima requerida')
+}
 
 
 
-// if(temp<0){
-//     console.log('Hace mucho frio')
-// }else if(temp<=10){
-//     console.log('Hace calor')
-// }else if(temp<=20){
-//     console.log('Esta agradable')
-// }else if(temp<=30){
-//     console.log('Hace calor')
-// }else {
-//     console.log('Hace mucho calor')
-// }
+let hola = document.querySelector('div')
+console.log("🚀 ~ div:", hola)
 
-let imagen = document.querySelector('div')
+hola.innerText('HOLA CHICOS DEL GRUPO 2')
 
-imagen = addEventListener('click', imagen.className = 'borde' )
+
+let temp = prompt('Ingrese la temperatura en grqados celcius')
+if(temp>30){
+    console.log('Hace mucho calor')
+}else if(temp>21){
+    console.log('Hace calor')
+}else if(temp>11){
+    console.log('Esta agradable')
+}else if(temp>0){
+    console.log('HAce frio')
+}else {
+    console.log('Hace mucho frio')
+}
+
+
+
+if(temp<0){
+    console.log('Hace mucho frio')
+}else if(temp<=10){
+    console.log('Hace calor')
+}else if(temp<=20){
+    console.log('Esta agradable')
+}else if(temp<=30){
+    console.log('Hace calor')
+}else {
+    console.log('Hace mucho calor')
+}
+
+// let imagen = document.querySelector('div')
+
+// imagen = addEventListener('click', imagen.className = 'borde' )
